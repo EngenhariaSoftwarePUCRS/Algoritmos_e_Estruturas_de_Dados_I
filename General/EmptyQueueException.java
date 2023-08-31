c3f8a10c76f12cd1d1ab9f93343c7f0120777f9c
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2023-08-31 04:08:07 -0300

    Initial Commit
    Included all files

---
+++ General/EmptyQueueException.java
@@ -0,0 +1,5 @@
+package General;
+
+public class EmptyQueueException extends RuntimeException {
+
+}
---
+++ General/Main.java
@@ -0,0 +1,9 @@
+package General;
+
+public class Main {
+    public static void main(String[] args) {
+        ColecaoEncadeada colecaoEncadeada = new ColecaoEncadeada();
+        colecaoEncadeada.addFirst("Segundo");
+        colecaoEncadeada.addFirst("Terceiro");
+    }
+}
